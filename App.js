cea41b88d71333130d3e496793cbcd168fc4011f
Ayesha Mukhtar Asad <168007496+Ayesha-Asadd@users.noreply.github.com>
2024-11-20 23:47:06 +0500

    updated app.js

--- App.js
+++ App.js
@@ -5,13 +5,13 @@ import LoginScreen from './screen/LoginScreen';
 import RegisterScreen from './screen/RegisterScreen';
 import Screen1 from './screen/Screen1';
 import Screen2 from './screen/Screen2';
-import HomeScreen from './screen/HomeScreen';
 import GenresScreen from './screen/GenresScreen';
 import FavScreen from './screen/Fav';
 import ViewAll from './screen/Viewall';
 import Genres2Screen from './screen/Genres2Screen';
 import Book2Detail1 from './screen/Mystrydet1'; 
 import Book2Detail2 from './screen/gonegirl'; 
+import MainComponent from './screen/HomeScreen';
 
 
 
@@ -26,13 +26,13 @@ const App = () => {
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Genres" component={GenresScreen} /> 
-        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Fav" component={FavScreen} />
         <Stack.Screen name="ViewAll" component={ViewAll} />
         <Stack.Screen name="Genres2Screen" component={Genres2Screen} />
         <Stack.Screen name="Book2Detail1" component={Book2Detail1} />
         <Stack.Screen name="Book2Detail2" component={Book2Detail2} />
         <Stack.Screen name="Jurassic" component={JurassicScreen} />
+        <Stack.Screen name="Home" component={MainComponent} />
       </Stack.Navigator>
 
       
